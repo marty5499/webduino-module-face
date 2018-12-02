0099e6e6955f42a7e4a2b8543d465baa7600cebb
Marty <marty@kingkit101.com>
2018-12-02 21:38:16 +0800

    fix weights url

--- face-blockly.js
+++ face-blockly.js
@@ -1,6 +1,7 @@
 +(async function (window, webduino) {
   'use strict';
   var MODEL_URL = "https://127.0.0.1:8080/weights/";
+  MODEL_URL = "https://marty5499.github.io/webduino-module-face/weights/";
   window.faceAPI = new webduino.module.face(MODEL_URL);
 
 }(window, window.webduino));
